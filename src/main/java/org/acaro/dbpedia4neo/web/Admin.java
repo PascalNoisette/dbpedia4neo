015649a532452b0d59cf648cd08e4066f1308629
Pascal <Pascal@Tuco>
2014-03-11 21:02:48 +0100

    add admin interface launcher

---
+++ src/main/java/org/acaro/dbpedia4neo/web/Admin.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.acaro.dbpedia4neo.web;
+
+import org.neo4j.kernel.EmbeddedGraphDatabase;
+import org.neo4j.kernel.GraphDatabaseAPI;
+import org.neo4j.server.WrappingNeoServerBootstrapper;
+
+/**
+ *
+ * @author Pascal
+ */
+public class Admin {
+
+    public static void main(String[] args) throws Exception {
+        final EmbeddedGraphDatabase rawGraph = new EmbeddedGraphDatabase("dbpedia4neo");
+        final WrappingNeoServerBootstrapper srv = new WrappingNeoServerBootstrapper((GraphDatabaseAPI) rawGraph);
+
+        // we need a clean shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("Shutting down...");
+                    rawGraph.shutdown();
+                    srv.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        srv.start();
+        System.in.read();
+
+    }
+}
