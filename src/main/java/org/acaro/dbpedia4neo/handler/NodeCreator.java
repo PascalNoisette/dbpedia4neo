9af30b16d0e4048601531b76832fb1a901ed0be9
Pascal <Pascal@Tuco>
2014-03-16 15:44:47 +0100

    add filter for creating only relevant node

--- src/main/java/org/acaro/dbpedia4neo/handler/NodeCreator.java
+++ src/main/java/org/acaro/dbpedia4neo/handler/NodeCreator.java
@@ -2,7 +2,33 @@
 
 package org.acaro.dbpedia4neo.handler;
 
-public class NodeCreator extends TripleHandler {
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class NodeCreator extends TripleHandler  {
+
+    private final HashMap<String, String> allowedNode = new HashMap<String, String>();
+    
+    public NodeCreator()
+    {
+        try {
+            Properties prop = new Properties();
+            prop.load(NodeCreator.class.getClassLoader().getResourceAsStream("filter.properties"));
+            Enumeration enuKeys = prop.keys();
+            while (enuKeys.hasMoreElements()) {
+                    String key = (String) enuKeys.nextElement();
+                    String value = prop.getProperty(key);
+                    allowedNode.put(key, value);
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(NodeCreator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     @Override
     void handleNodePropertyRead(String node, String predicate, String propertyName) {
@@ -10,6 +36,8 @@ public class NodeCreator extends TripleHandler {
 
     @Override
     void handleRelationshipRead(String node, String predicate, String relatedNodeName) {
-        neo.createNode(node);
+        if (allowedNode.containsKey(relatedNodeName) && allowedNode.get(relatedNodeName).equals(predicate)) {
+            neo.createNode(node);
+        }
     }
 }
