cfb17a6c5d6dd478165d5212d0d41777fad84468
Pascal <Pascal@Tuco>
2014-03-21 10:49:21 +0100

    add disclamer licence to source file (default gpl)

--- src/main/java/org/acaro/dbpedia4neo/inserter/DBpediaLoader.java
+++ src/main/java/org/acaro/dbpedia4neo/inserter/DBpediaLoader.java
@@ -1,3 +1,20 @@
+/**
+ *   This file is part of `PascalNoisette/dbpedia4neo` project
+ *   Copyright (C) 2014  <netpascal0123@aol.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.acaro.dbpedia4neo.inserter;
 
 import java.io.BufferedInputStream;
@@ -11,7 +28,6 @@ import org.openrdf.rio.RDFParseException;
 import org.openrdf.rio.ntriples.NTriplesParser;
 
 import java.io.InputStream;
-import org.acaro.dbpedia4neo.handler.NodeCreator;
 import org.acaro.dbpedia4neo.handler.TripleHandler;
 import org.acaro.dbpedia4neo.inserter.db.BatchGraph;
 import org.openrdf.model.impl.ValueFactoryImpl;
